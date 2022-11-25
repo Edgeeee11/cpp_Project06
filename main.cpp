3329334ebe0da18d0d768cbacc2a296a9f387f92
Edgeeee11 <arsenykurenkov@gmail.com>
2022-11-25 19:54:23 +0300

    Homework

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+
+main()
+{
+int i=1;
+int a=0;
+int b=1;
+int sum=0;
+int c=5;
+
+while(i<10)
+{
+	printf("Hello\n");
+	i=i+1;
+}
+while(a<15)
+{
+	printf("%i\n", a);
+	a=a+1;
+	
+}
+while(b<15){
+	printf("%i ", b);
+	b=b+2;
+}
+while(c<15)
+{
+	sum=sum+c;
+	printf("%i+",c);
+	c=c+1;
+}
+sum=sum+c;
+printf("%i=%i", c, sum);
+}
+
+
+
+
+
